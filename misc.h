00cc32f959a6c777031a0743b11cf9b3d4e00689
pkishi <rsdharmaji@gmail.com>
2020-06-13 02:06:58 -0700

    add misc.h

---
+++ misc.h
@@ -0,0 +1,14 @@
+
+#ifndef MISC_H
+#define MISC_H
+
+#define VERSION "0.01a"
+#define TICKS_TO_SEC 9
+#define MIN_NOTE_HEIGHT 14 // is this needed anymore?
+#define MIN_NOTE_IDX 21
+#define MAX_NOTE_IDX 108
+#define NOTE_RANGE 88
+#define WIN_HEIGHT 720
+#define WIN_WIDTH 1280
+
+#endif
