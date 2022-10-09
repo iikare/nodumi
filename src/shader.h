e315f6637fdbc97a0dd50eab056687a5cb242290
iikare <13582030+iikare@users.noreply.github.com>
2022-10-09 16:33:39 -0700

    `setShaderValue()` type safety

--- src/shader.h
+++ src/shader.h
@@ -15,6 +15,7 @@
 using std::map;
 using std::vector;
 using std::string;
+using std::to_string;
 using std::is_same;
 
 
@@ -33,9 +34,17 @@ class shaderData {
                     is_same<T, float>::value, 
                     "invalid type passed to uniform");
 
+      constexpr int ufType = is_same<T, Vector2>::value ? SHADER_UNIFORM_VEC2 :
+                             is_same<T, Vector3>::value ? SHADER_UNIFORM_VEC3 :
+                             is_same<T, float>::value ? SHADER_UNIFORM_FLOAT : 255; // unused value
+
       auto it = typeMap.find(uf);
       if (it == typeMap.end()) {
         logW(LL_WARN, name, "- attempt to modify non-existent uniform:", uf);
+        return;
+      }
+      if (ufType != it->second) {
+        logW(LL_WARN, name, "- expecting uniform of type", to_string(it->second)+",", "not", ufType);
         return;
       }
       SetShaderValue(shader, GetShaderLocation(shader, uf.c_str()), &val, it->second);
