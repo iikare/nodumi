d14633df996b5552263db372f44bbebc0e334438
iikare <13582030+iikare@users.noreply.github.com>
2022-11-16 20:20:24 -0800

    dependency compilation user-triggered

--- tool/cross.sh
+++ tool/cross.sh
@@ -2,7 +2,11 @@
 
 #raylib library cross-compile script
 
-raylibdir=./dpd/raylib/src
-
-make -j $(nproc) -C $raylibdir clean
-make -j $(nproc) -C $raylibdir CC=x86_64-w64-mingw32-gcc PLATFORM=PLATFORM_DESKTOP PLATFORM_OS=WINDOWS RAYLIB_LIBTYPE=STATIC
+read -q "e?cross compile raylib? (y/n) "
+echo
+if [[ $e =~ ^[Yy]$ ]]
+then
+  raylibdir=./dpd/raylib/src
+  make -j $(nproc) -C $raylibdir clean
+  make -j $(nproc) -C $raylibdir CC=x86_64-w64-mingw32-gcc PLATFORM=PLATFORM_DESKTOP PLATFORM_OS=WINDOWS RAYLIB_LIBTYPE=STATIC
+fi
