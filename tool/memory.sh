aae58073de7e4dcac781e713550377f7999797b4
iikare <13582030+iikare@users.noreply.github.com>
2024-01-14 23:26:46 -0800

    cleanup

--- tool/memory.sh
+++ tool/memory.sh
@@ -9,12 +9,12 @@ trap sigint_handler SIGINT
 source ./tool/color.sh
 
 # this script enables memory profiling via the "heaptrack" tool
-if [[ $# -ne 1 ]]
+if [[ $# -ne 1 && $# -ne 2 ]]
 then
   cecho $C_F "need 1 argument, not $#"
   return -1
 fi
 
-heaptrack ./bin/nodumi $1 
+heaptrack ./bin/nodumi $1 $2 
 rm -f *.zst
 
