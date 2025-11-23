67b896877d75e2f0dc4e0962a306b3d8ca702379
iikare <13582030+iikare@users.noreply.github.com>
2025-11-22 19:44:34 -0800

    refine particle step offset

--- src/particle_inst.cc
+++ src/particle_inst.cc
@@ -5,7 +5,8 @@ void particle::update(double t_step, double zoom, double ratio) {
   r_life -= t_step * rand() / float(RAND_MAX);
   vel = 2 * r_life / t_life * (rand() / float(RAND_MAX));
 
-  x += ratio * zoom * 500 * vel * t_step * cos(angle * M_PI / 180.0);
+  auto x_off = ratio * zoom * 500 * vel * t_step * cos(angle * M_PI / 180.0);
+  x += (x_off > 0) ? 0.2 * x_off : x_off;
   y += ratio * zoom * 500 * vel * t_step * sin(angle * M_PI / 180.0);
 
   // logQ(100*vel*t_step*sin(angle*M_PI/180.0));
